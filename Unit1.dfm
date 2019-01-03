861527cd7caf3967794e062262a8b1d6b89e3bb3
ManoloGC <kileno@hotmali.com>
2019-01-03 09:46:51 +0100

    fffff

--- Unit1.dfm
+++ Unit1.dfm
@@ -13,4 +13,10 @@ object Form1: TForm1
   OldCreateOrder = False
   PixelsPerInch = 96
   TextHeight = 13
+  object ClientDataSet1: TClientDataSet
+    Aggregates = <>
+    Params = <>
+    Left = 145
+    Top = 100
+  end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -4,12 +4,13 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.DB, Datasnap.DBClient;
 
 type
   TForm1 = class(TForm)
   private
-    { Private declarations }
+    { Private declarations
+    ClientDataSet1: TClientDataSet;}
   public
     { Public declarations }
   end;
